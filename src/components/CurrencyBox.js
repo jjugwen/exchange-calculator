30c391d468ce4985ecdbd90f397cc16923775a69
jjugwen <yjgwen@gmail.com>
2023-03-08 00:08:47 +0900

    Update CurrencyBox.js

--- src/components/CurrencyBox.js
+++ src/components/CurrencyBox.js
@@ -2,10 +2,8 @@ import React from "react";
 
 function CurrencyBox(props) {
   const { opt, selectedNation, handleSelected, money, onChange,} = props;
-  // const amount = money.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
   let amount
-  amount = money.toLocaleString('ko-KR', {maximumFractionDigits: 2})
-  // const amount = parseFloat(comma).toFixed(2);
+  amount = money.toLocaleString('ko-KR', {maximumFractionDigits: 2});
   return (
       <div className="input-box">
       <input type="text" min={0} onChange={onChange} value={amount} aria-label={selectedNation}/>
